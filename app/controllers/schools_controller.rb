85ddffd45cde9a917d5addcdc8fd3fce7161b2a6
aenrione <aenrione@protonmail.com>
2020-10-11 14:19:01 -0300

    feat(controllers): add initial base_models_controllers and html views

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,13 @@
+class SchoolsController < ApplicationController
+  def new; end
+
+  def index; end
+
+  def edit; end
+
+  def update; end
+
+  def create; end
+
+  def destroy; end
+end
---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,13 @@
+class TeachersController < ApplicationController
+  def new; end
+
+  def index; end
+
+  def edit; end
+
+  def update; end
+
+  def create; end
+
+  def destroy; end
+end
