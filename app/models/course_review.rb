89e1605fc3014a9f799047b194fd89e3e4d32b5b
Alfredo Enrione <60358967+aenrione@users.noreply.github.com>
2020-10-18 02:25:48 -0300

    feat(validations and gemfile): add model validations and pronto to gemfile

--- app/models/course_review.rb
+++ app/models/course_review.rb
@@ -4,7 +4,6 @@ class CourseReview < ApplicationRecord
   validates :general_comment, :course_id, :user_id, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0,
                                      less_than_or_equal_to: 7 }, presence: true
-  validates_with UniqueCourseReviewPerUserValidator
 end
 
 # == Schema Information
